eaa7efbba2319d624e9287ac4ce30b4bb46b4f33
ElishaShmalo <47370690+ElishaShmalo@users.noreply.github.com>
2021-06-05 21:21:32 -0400

    Update database.py

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQL_URL = "sqlite:///lotrq_app/data.db"
+SQL_URL = "sqlite:///data.db"
 
 engine = create_engine(SQL_URL, connect_args={"check_same_thread": False})
 
